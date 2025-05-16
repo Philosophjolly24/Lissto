5ee5a55c073c6c40eef213df5abf4e965d883ec7
CoffeeAddict25 <ahnsanou.ahnoux@gmail.com>
2025-05-16 11:55:23 +0200

    changed name from Listoria to Lissto

--- vite.config.ts
+++ vite.config.ts
@@ -9,8 +9,8 @@ export default defineConfig({
       registerType: "autoUpdate",
       includeAssets: ["favicon.ico", "robots.txt", "apple-touch-icon.png"],
       manifest: {
-        name: "Listoria",
-        short_name: "Listoria",
+        name: "Lissto",
+        short_name: "Lissto",
         description: "Offline-capable shopping checklist app",
         start_url: "/",
         display: "standalone",
